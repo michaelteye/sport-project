6e8771283f7605242caf65087a03123d9c5882af
Everich Bernz <megamindcooperations@gmail.com>
2022-10-11 22:18:43 +0000

    fixed css  issues

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -3,6 +3,7 @@ import Container from "../layout/Container";
 import Header from "../components/Header";
 import Footer from "../layout/Footer";
 import productImageLarge from "../assets/images/product-large.jpg"
+import shoppingCart from "../assets/icons/png/shopping-cart-white.png"
 export default function Product() {
   return (
     <>
@@ -15,6 +16,26 @@ export default function Product() {
         </div>
         <a class="prev" ></a>
         <a class="next"></a>
+        <div class="row">
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge}  alt="The Woods"/>
+            </div>
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge} alt="Cinque Terre"/>
+            </div>
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge}  alt="Mountains and fjords"/>
+            </div>
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge}  alt="Northern Lights"/>
+            </div>
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge}  alt="Nature and sunrise"/>
+            </div>
+            <div class="column">
+              <img class="demo cursor" src={productImageLarge}  alt="Snowy Mountains"/>
+            </div>
+          </div>
     </section>
       <section class="text">
         <div class="affect">
@@ -25,7 +46,7 @@ export default function Product() {
                 <p class="save bake">Save 67%</p>
                 <p class="fav bake"><span><img src="" alt=""/></span> Add to Favorites</p>
             </div>
-            <div class="des" style={{maxWidth:"200px"}}>
+            <div class="des">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum labore perferendis asperiores, ipsum ullam voluptate architecto dolore? Molestias 
                 inventore labore id necessitatibus ab aspernatur aut obcaecati nam nostrum? Fuga, facilis?</p>
             </div>
@@ -48,36 +69,20 @@ export default function Product() {
             <div class="fill">
                 <p>Quantity :</p>
                 <input type="number" class="change value" placeholder="1"/>
-            </div>
-            
+            </div>  
+        </div>
             <div class="btn">
-                <p>add to cart</p>
-                <button><img src="icons/png/shopping-cart-white.png" alt=""/></button>
+                <button>
+                  <span>Add to cart</span>
+                  <div className="btn-image">
+                <img src={shoppingCart} alt=""/>
+                  </div>
+                </button>
 
             </div>
-        </div>
     </section>
       </div>
-           <div class="row">
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge}  alt="The Woods"/>
-            </div>
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge} alt="Cinque Terre"/>
-            </div>
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge}  alt="Mountains and fjords"/>
-            </div>
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge}  alt="Northern Lights"/>
-            </div>
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge}  alt="Nature and sunrise"/>
-            </div>
-            <div class="column">
-              <img class="demo cursor" src={productImageLarge}  alt="Snowy Mountains"/>
-            </div>
-          </div>
+         
       </Container>
       <Footer />
     </>
